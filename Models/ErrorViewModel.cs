e2be48f0a9e542a30a667500a198c740c82c3dbc
Phùng Đức Anh <183066484+DucAnhSCY@users.noreply.github.com>
2025-06-26 09:53:32 +0700

    haha

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieBookingTIcket.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
